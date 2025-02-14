4f48b9b444571ee1c51bf66b1d1291f26df8d4ac
razielnavarro <elrazand@gmail.com>
2025-02-14 11:39:42 -0500

    chore: remove unnecesary imports and fix bindings

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,8 @@
 import { createMiddleware } from "hono/factory";
-import { jwt } from "hono/jwt";
 
 import { Env } from "./common/types";
 
-export const apiKeyMiddleware = createMiddleware<{Bindings: Env}>(async (c, next) => {
+export const apiKeyMiddleware = createMiddleware<Env>(async (c, next) => {
   const apiKey = c.req.header("api-key");
   if (!apiKey || apiKey !== c.env.API_KEY) {
     return c.json({ error: "No autorizado" }, 401);
