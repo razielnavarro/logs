cf8ae1b6396ce95b811868b95c594ea91c99ee3c
razielnavarro <elrazand@gmail.com>
2025-02-18 09:50:19 -0500

    feat: add IP address field to login schema for enhanced logging

--- src/schemas/login.schema.ts
+++ src/schemas/login.schema.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 export const loginSchema = z.object({
     user_id: z.string(),
     success: z.boolean(),
+    ip: z.string(),
     country: z.string(),
     user_agent: z.string(),
 });
