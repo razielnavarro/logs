7271b50b2d8bc41b26b200930cbdd0a8a419f13e
razielnavarro <elrazand@gmail.com>
2025-02-18 09:50:26 -0500

    feat: rename user table to user_logs and establish relations with login_logs and event_logs

--- src/entities/user_logs.entity.ts
+++ src/entities/user_logs.entity.ts
@@ -1,12 +1,15 @@
 import { sql } from 'drizzle-orm';
 import { generateId } from '../utils/random';
+import { loginLogs } from './login_logs.entity';
+import { eventLogs } from './event_logs.entity';
+import { relations } from 'drizzle-orm';
 import { 
     sqliteTable, 
     text, 
     integer 
 } from 'drizzle-orm/sqlite-core';
 
-export const userLogs = sqliteTable('user', {
+export const userLogs = sqliteTable('user_logs', {
 	id: text("id").primaryKey(),
 	email: text(),
 	full_name: text(),
@@ -14,3 +17,8 @@ export const userLogs = sqliteTable('user', {
 	sucursal_code: text(),
 	is_premium: integer('is_premium', { mode: 'boolean' }).notNull().default(false),
 });
+
+export const userRelations = relations(userLogs, ({ many }) => ({
+	loginLogs: many(loginLogs),
+	eventLogs: many(eventLogs),
+}));
