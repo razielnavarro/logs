bf5e14c6c468bf322be59df8cf9b3da6b01a83c8
razielnavarro <elrazand@gmail.com>
2025-02-18 09:48:59 -0500

    refactor: simplify login controller by removing unnecessary ID and IP generation

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -3,24 +3,20 @@ import { drizzle } from 'drizzle-orm/d1';
 import { loginLogs } from '../entities';
 import { Env } from '../common/types';
 import { loginSchema } from "../schemas/login.schema";
-import { getConnInfo } from 'hono/cloudflare-workers'
 import { apiKeyMiddleware } from "../middleware";
-import { generateId } from '../utils/random';
+
 
 export const loginController = new Hono<Env>();
 
 loginController.post('/', apiKeyMiddleware, async (c) => {
     const db = drizzle(c.env.DB);
     const data = await c.req.json();
-    const id = generateId();
-    const info = getConnInfo(c);
-    const ip = info.remote.address || 'Unknown';
-    const inputData = { ...data, id, ip };
-    const parsed = loginSchema.safeParse(inputData);
+    const parsed = loginSchema.safeParse(data);
     if (!parsed.success) {
         return c.json({ error: parsed.error }, 400);
     }
-    const [record] = await db.insert(loginLogs).values(inputData).returning();
+    const [record] = await db.insert(loginLogs).values(data).returning();
+
     return c.json({ record });
 });
 
